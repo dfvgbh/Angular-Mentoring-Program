506feb93267800db87ea9b3143868cac2f3177d1
Anton Dmytriichuk <anton_dmytriichuk@epam.com>
2017-12-05 12:16:44 +0200

    Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { CoursesModule } from './modules/courses/courses.module';
-
 
 @NgModule({
   declarations: [
