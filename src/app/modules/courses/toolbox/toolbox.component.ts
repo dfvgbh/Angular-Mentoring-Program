02e3b5458e01800d1568c06818b42ababf29365a
Anton Dmytriichuk <anton_dmytriichuk@epam.com>
2017-12-05 03:10:46 +0200

    add missed files

---
+++ src/app/modules/courses/toolbox/toolbox.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'amp-toolbox',
+  templateUrl: './toolbox.component.html',
+  styleUrls: ['./toolbox.component.scss']
+})
+export class ToolboxComponent implements OnInit {
+  searchQuery = 'Search';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  find() {
+    console.log(`FIND: ${this.searchQuery}`);
+  }
+
+}
